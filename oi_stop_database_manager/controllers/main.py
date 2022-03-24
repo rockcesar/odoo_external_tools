d81bd51e832b148d2b8f19165f5ab6ef8830f18b
ccordero <cesar.cordero.r@gmail.com>
2022-03-24 07:28:20 -0400

    Upgrade

--- oi_stop_database_manager/controllers/main.py
+++ oi_stop_database_manager/controllers/main.py
@@ -4,8 +4,53 @@ Created on Nov 18, 2018
 @author: Zuhair Hammadi
 '''
 from odoo.addons.web.controllers.main import Database
-from odoo import http
+from odoo import http, tools
 from werkzeug.exceptions import BadRequest
+
+import babel.messages.pofile
+import base64
+import copy
+import datetime
+import functools
+import glob
+import hashlib
+import io
+import itertools
+import jinja2
+import json
+import logging
+import operator
+import os
+import re
+import sys
+import tempfile
+
+import werkzeug
+import werkzeug.exceptions
+import werkzeug.utils
+import werkzeug.wrappers
+import werkzeug.wsgi
+from collections import OrderedDict, defaultdict, Counter
+from werkzeug.urls import url_encode, url_decode, iri_to_uri
+from lxml import etree
+import unicodedata
+
+
+import odoo
+import odoo.modules.registry
+from odoo.api import call_kw, Environment
+from odoo.modules import get_module_path, get_resource_path
+from odoo.tools import image_process, topological_sort, html_escape, pycompat, ustr, apply_inheritance_specs, lazy_property, float_repr
+from odoo.tools.mimetypes import guess_mimetype
+from odoo.tools.translate import _
+from odoo.tools.misc import str2bool, xlsxwriter, file_open
+from odoo.tools.safe_eval import safe_eval, time
+from odoo.http import content_disposition, dispatch_rpc, request, serialize_exception as _serialize_exception, Response
+from odoo.exceptions import AccessError, UserError, AccessDenied
+from odoo.models import check_method_name
+from odoo.service import db, security
+
+_logger = logging.getLogger(__name__)
 
 class MyDatabase(Database):
 
